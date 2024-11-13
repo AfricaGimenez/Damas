f8b381a7e1addc9e273834d62aa437bf9dec3b49
AfricaGimenez <gimenezferrona22@gmail.com>
2024-11-13 17:31:51 +0100

    He creado los métodos get y set para los atributos. He recordado que para el método set se debe tener en cuenta que los valores indicados sean correctos (las filas van del 1 al 8 -ambos inclusive- y las columnas de la 'a' a la 'h' -ambas inclusive-) y si no, que se lanza una excepción del tipo IllegalArgumentExceptioncon el mensaje adecuado. En caso de que la posición no sea la correcta no deben modificarse los atributos de la dama.

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
@@ -0,0 +1,18 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public enum Color {
+    BLANCO("Blanco"),
+    NEGRO("Negro");
+    private String cadenaAMostrar;
+
+    Color(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion{" +
+                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posición.java
@@ -0,0 +1,28 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public class Posición {
+    private int Fila;
+    private char Columna;
+
+    public int getFila() {
+        return Fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8) {
+            throw new IllegalArgumentException("Las filas solo pueden estar entre 1 y 8");
+        }
+        Fila = fila;
+    }
+
+    public char getColumna() {
+        return Columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna < 1 || columna > 8) {
+            throw new IllegalArgumentException("Las filas solo pueden estar entre 1 y 8");
+        }
+        Columna = columna;
+    }
+}
