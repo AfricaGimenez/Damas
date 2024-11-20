ece931831c0b648fe86489e7a8e900fc0b803043
AfricaGimenez <gimenezferrona22@gmail.com>
2024-11-20 12:39:57 +0100

    He creado el método elegirOpcionMenu que mostrará un mensaje para que elijamos una opción del menú anteriormente creado y nos pedirá que introduzcamos por teclado la opción hasta que ésta sea valida

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -13,4 +13,15 @@ public class Consola {
         System.out.println("4. Salir");
     }
 
+    // Metodo para elegir una opción del menú
+    public static int elegirOpcionMenu() {
+        int opcion;
+        do{
+            System.out.println("Escoge entre las opciones (1 - 4)");
+            opcion = Entrada.entero();
+        } while (opcion < 1 || opcion > 4);
+        return opcion;
+    }
+
+
 }
