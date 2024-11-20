cd517b503aeb53ef28434d7e5ab33509cd518ada
AfricaGimenez <gimenezferrona22@gmail.com>
2024-11-20 12:32:09 +0100

    He creado el método mostrarMenu que mostrará el menú con las opciones de nuestra aplicación: crear dama por defecto, crear dama eligiendo el color, mover y salir

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -4,4 +4,13 @@ public class Consola {
     public Consola() {
     }
 
+    // Metodo para mostrar el menú de opciones
+    public static void mostrarMenu() {
+        System.out.println("Menú de opciones:");
+        System.out.println("1. Crea una dama por defecto");
+        System.out.println("2. Crea una dama eligiendo el color");
+        System.out.println("3. Mover la dama");
+        System.out.println("4. Salir");
+    }
+
 }
